5c11bca30c03f2c5149906c19638b1188a5493dc
Musan-HaoWu <musan.haowu@gmail.com>
2025-11-17 14:35:08 +0000

    Deploying to gh-pages from @ Musan-HaoWu/Musan-HaoWu.github.io@e28a866468e37976452f05d6a434f692286d37a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-hao-wu-formal-verification-ios",
+    title: "Hao Wu (Formal Verification @IoS)",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
